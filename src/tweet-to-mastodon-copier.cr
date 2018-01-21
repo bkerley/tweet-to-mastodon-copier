9285a70e86c2d4e1a195122769ad52dd720ce479
Bryce Kerley <bkerley@brycekerley.net>
2018-01-21 13:59:44 -0500

    initial

---
+++ src/tweet-to-mastodon-copier.cr
@@ -0,0 +1,6 @@
+require "./tweet-to-mastodon-copier/*"
+
+# TODO: Write documentation for `Tweet::To::Mastodon::Copier`
+module Tweet::To::Mastodon::Copier
+  # TODO: Put your code here
+end
---
+++ src/tweet-to-mastodon-copier/version.cr
@@ -0,0 +1,3 @@
+module Tweet::To::Mastodon::Copier
+  VERSION = "0.1.0"
+end
